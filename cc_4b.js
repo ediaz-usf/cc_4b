026206c9197f8db634eb0d7b87312e3cca828f60
Ernesto Diaz <diaz37@usf.edu>
2025-09-17 19:40:58 -0400

    initial commit

---
+++ cc_4b.js
@@ -0,0 +1,8 @@
+let products = [
+    {sku:'001', name:'apple', category:'fruit', price: 1, inventory: 10},
+    {sku:'002', name:'banana', category:'fruit', price: 0.5, inventory: 50},
+    {sku:'003', name:'lettuce', category:'vegetable', price: 2, inventory: 20},
+    {sku:'004', name:'orange juice', category:'juice', price: 6, inventory: 25},
+    {sku:'005', name:'soap', category:'soap', price: 10, inventory: 30},
+]
+
