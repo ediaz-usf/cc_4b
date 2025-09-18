012978e0f326105fd85e97a2f3ffc99aeb796767
Ernesto Diaz <diaz37@usf.edu>
2025-09-17 20:05:44 -0400

    second commit

--- cc_4b.js
+++ cc_4b.js
@@ -6,3 +6,50 @@ let products = [
     {sku:'005', name:'soap', category:'soap', price: 10, inventory: 30},
 ]
 
+for (let product of products) {
+
+    switch (product.category) {
+        case 'fruit':
+            discount = .20; // 20% discount
+            break;
+        case 'vegetable':
+            discount = 0.15;   // 15% discount
+            break;
+        case 'juice':
+            discount = 0.1;    // 10% discount
+            break;
+        case 'soap':
+            discount = 0.1;   // 10% discount
+            break;
+        default:
+            discount = 1;  // no discount
+            break;
+    }
+     // Place promoPrice integration here
+     let promoPrice = product.price * (1 - discount);
+     console.log(`The promo price for ${product.name} is $${promoPrice}`);
+} 
+
+let customerType = ['regular', 'student', 'senior'];
+
+for (let product of products) {
+
+    if (customerType === 'student') {
+            couponCode = 0.05; // 5% discount
+    } else if (customerType === 'senior') {
+            couponCode =  0.07;   // 7% discount
+    }
+
+    let finalPrice = promoPrice * (1 - couponCode);
+    console.log(`The final price for ${product.name} is $${finalPrice}`);
+}
+
+let couponCode = ['SAVE10', 'FREESHIP'];
+
+if (couponCode === 'SAVE10') {
+        if (promoPrice >= 50) {
+            promoPrice = promoPrice - 10; // $10 off for orders over $50
+        }
+} else if (couponCode === 'FREESHIP') {
+        promoPrice = promoPrice - 5; // $5 off for free shipping
+}
